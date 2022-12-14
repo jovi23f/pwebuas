fd326dfd833245cbd0fdae862fb3f1a5de7be874
jovi23f <100178380+jovi23f@users.noreply.github.com>
2022-12-14 19:28:17 +0700

    Add files via upload

---
+++ view.blade.php
@@ -0,0 +1,63 @@
+@extends('fidela')
+
+@section('atas')
+    <a class="btn btn-primary" href="/pegawai">Kembali</a>
+@endsection
+
+@section('bawah')
+    @foreach ($pegawai as $p)
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="id" >Pegawai ID:</label>
+                <div class="col-sm-10">
+                    <label class="control-label">{{ $p->pegawai_id }}</label>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="nama" >Nama:</label>
+                <div class="col-sm-10">
+                    <label class="control-label">{{ $p->pegawai_nama }}</label>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="jabatan">Jabatan</label>
+                <div class="col-sm-10">
+                    <label class="control-label">{{ $p->pegawai_jabatan }}</label>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="umur">Umur</label>
+                <div class="col-sm-10">
+                    <label class="control-label">{{ $p->pegawai_umur }}</label>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="alamat">Alamat</label>
+                <div class="col-sm-10">
+                    <label class="control-label">
+                        {{ $p->pegawai_alamat }}
+                    </label>
+                </div>
+            </div>
+
+    @endforeach
+
+    {{-- <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+	<h3>Edit Pegawai</h3>
+
+	<a href="/pegawai"> Kembali</a>
+
+	<br/>
+	<br/>
+
+	@foreach ($pegawai as $p)
+	<form action="/pegawai/update" method="post">
+		{{ csrf_field() }}
+		<input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
+		Nama <input type="text" required="required" name="nama" value="{{ $p->pegawai_nama }}"> <br/>
+		Jabatan <input type="text" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}"> <br/>
+		Umur <input type="number" required="required" name="umur" value="{{ $p->pegawai_umur }}"> <br/>
+		Alamat <textarea required="required" name="alamat">{{ $p->pegawai_alamat }}</textarea> <br/>
+		<input type="submit" value="Simpan Data">
+	</form>
+	@endforeach --}}
+@endsection
